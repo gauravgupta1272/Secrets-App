6d6d084e71117a2513625cb8821663cd22123985
Gaurav <U20EC179@eced.svnit.ac.in>
2022-12-01 20:17:25 +0530

    level 4 salting+hashing

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-const md5 = require("md5");         //type of hashing technique
+const bcrypt = require('bcrypt');
+const saltRounds = 1;
 
 const app = express();
 app.use(express.static("public"));
@@ -18,13 +18,34 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-
-// userSchema.plugin(encrypt,{ secret: process.env.SECRET, encryptedFields: ['password']});    //encryption
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
+});
+
+app.route("/register")
+.get(function (req, res) {
+    res.render("register");
+  })
+.post(function (req, res) {
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        console.log(hash);
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+            
+          });
+      
+          newUser.save(function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render("secrets");
+            }
+          });
+    });    
 });
 
 app.route("/login")
@@ -33,39 +54,24 @@ app.route("/login")
   })
 .post(function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, function (err, foundUser) {
       if (err) {
         console.log(err);
       } else {
         if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets");
-          }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result===true){
+                    res.render("secrets");
+                }
+            });
         }
       }
     });
   });
 
-app.route("/register")
-.get(function (req, res) {
-    res.render("register");
-  })
-.post(function (req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password)
-    });
 
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
 
 app.listen(3000, function () {
   console.log("Server Running on Port 3000");
