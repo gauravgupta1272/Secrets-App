9e8fc64699f4cfbc6deb332d0254197c8fdbe2c0
Gaurav <U20EC179@eced.svnit.ac.in>
2022-12-01 15:18:35 +0530

    Level2 with env variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -5,7 +6,6 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +17,8 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "ThisisaLittleSecret.";
-userSchema.plugin(encrypt,{ secret: secret, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt,{ secret: process.env.SECRET, encryptedFields: ['password']});    //encryption
 
 const User = new mongoose.model("User", userSchema);
 
@@ -26,12 +26,11 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-app
-  .route("/login")
-  .get(function (req, res) {
+app.route("/login")
+.get(function (req, res) {
     res.render("login");
   })
-  .post(function (req, res) {
+.post(function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
@@ -48,12 +47,11 @@ app
     });
   });
 
-app
-  .route("/register")
-  .get(function (req, res) {
+app.route("/register")
+.get(function (req, res) {
     res.render("register");
   })
-  .post(function (req, res) {
+.post(function (req, res) {
     const newUser = new User({
       email: req.body.username,
       password: req.body.password,
